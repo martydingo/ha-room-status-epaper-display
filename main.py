ed47193a42d2d3394f3d55847a2da8187e3c766f
Martin George <marty@dingo.sh>
2021-01-16 19:23:52 +0000

    Fixes

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ draw.line(((dispWidth/10,((dispHeight/3)+48)),(dispWidth/5,((dispHeight/3)+48)))
 Lights = crawlRoomLights(roomName)
 for light in Lights:
     lightDict = ha.entities.entity_id(light)
-    draw.text((0, ((dispHeight/3+72)+(Lights.index(entity_id)*26))), lightDict['attributes']['friendly_name'] + "is switched " + lightDict['state'],
+    draw.text((0, ((dispHeight/3+72)+(Lights.index(light)*26))), lightDict['attributes']['friendly_name'] + "is switched " + lightDict['state'],
               font=lightsFont, fill=0, anchor='ls')
 
 ## Push to Display
