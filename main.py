40884d56a2aee30809dd0f3ecddeb071d0a6b1cc
Martin George <marty@dingo.sh>
2021-01-16 20:02:32 +0000

    Media Inital Commit

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import libs.epd7in5_V2.epd7in5_V2 as epd7in5_V2
 from PIL import Image, ImageDraw, ImageFont
 import os 
 
+
+def crawlMedia():
+
+                                print(entities['attributes']['friendly_name'] + ' is currently ' + entities['state'])
+                                try:
+                                    if(entities['attributes']['media_title']):
+                                        print('--- ' + entities['attributes']['media_title'] + ' - ' + entities['attributes']['media_artist']+' ---')
+                                except KeyError:
+                                    continue
+
 def crawlRoomLights(roomName):
     haStates = ha.entities.all()
     roomLights = []
@@ -10,7 +20,19 @@ def crawlRoomLights(roomName):
         if('light' in entities['entity_id']):
             if((str(roomName).replace(' ','_').lower() + '_') in entities['entity_id']):
                 roomLights.append(entities['entity_id'])
-                #roomLights.append(entities['attributes']['friendly_name'])
+    return roomLights
+
+def crawlRoomMedia(roomName):
+    haStates = ha.entities.all()
+    roomMedia = []
+    for entities in haStates:
+        if('media_player' in entities['entity_id']):
+            if('unavailable' not in entities['state']):
+                if('off' not in entities['state']):
+                    if('unknown' not in entities['state']):
+                        if('idle' not in entities['state']):
+                            if('Lounge Room' not in entities['attributes']['friendly_name']):
+                                roomMedia.append(entities['entity_id'])
     return roomLights
 
 dispWidth = 800
@@ -21,8 +43,6 @@ lightsTitleFont = ImageFont.truetype((os.getcwd()+"/fonts/BebasNeue-Regular.ttf"
 lightsFont = ImageFont.truetype((os.getcwd()+"/fonts/BebasNeue-Regular.ttf"),24)
 roomName = "Living Room"
 
-
-haStates = ha.entities.all()
 
 
 try:
@@ -51,6 +71,24 @@ for light in Lights:
     draw.text((10, ((dispHeight/4+72)+(Lights.index(light)*26))), lightDict['attributes']['friendly_name'] + " is switched " + lightDict['state'],
               font=lightsFont, fill=0, anchor='ls')
 
+## List of Media
+draw.text((10, dispHeight/1.25), "Media",
+          font=lightsTitleFont, fill=0, anchor='ls')
+draw.line(((10,((dispHeight/1.25)+48)),(dispWidth/6,((dispHeight/1.25)+48))))
+Media = crawlRoomMedia(roomName)
+for media in Media:
+    mediaDict = ha.entities.entity_id(media)
+    draw.text((10, ((dispHeight/4+72)+(Media.index(light)*26))), mediaDict['attributes']['friendly_name'] + " is currently " + mediaDict['state'],
+              font=MediaFont, fill=0, anchor='ls')
+    try:
+        if(mediaDict['attributes']['media_title']):
+            currentlyPlaying=('--- ' + mediaDict['attributes']['media_title'] + ' - ' + mediaDict['attributes']['media_artist']+' ---')
+            draw.text((10, ((dispHeight/1.25+72)+(Media.index(light)*52))), currentlyPlaying,
+                      font=MediaFont, fill=0, anchor='ls')
+    except KeyError:
+        continue
+
+    
 ## Push to Display
 try:
     disp.display(disp.getbuffer(image))
