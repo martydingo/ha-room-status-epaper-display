e03bc4d1aa2214c2ab229a18bc8851fb098de231
Martin George <marty@dingo.sh>
2021-01-16 17:12:24 +0000

    Fixes

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import libs.ha_api_lib as ha
 import libs.epd7in5_V2.epd7in5_V2 as epd7in5_V2
 
 disp = epd7in5_V2.EPD()
-
-disp.clear()
+dispInitRC = disp.init()
+print(dispInitRC)
+disp.Clear()
 
 print(ha.entities.entity_id("light.living_room_desk_lamp"))
