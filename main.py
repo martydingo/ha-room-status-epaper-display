e23c20f13b708bdbd1534b1346b7db6b4df0687f
Martin George <marty@dingo.sh>
2021-01-16 17:32:25 +0000

    PoC Initial Commit

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import libs.ha_api_lib as ha
 import libs.epd7in5_V2.epd7in5_V2 as epd7in5_V2
+import PIL
+import os 
 
-disp = epd7in5_V2.EPD()
-dispInitRC = disp.init()
-print(dispInitRC)
-disp.Clear()
+dispWidth = 800
+dispHeight = 480
 
-print(ha.entities.entity_id("light.living_room_desk_lamp"))
+font = ImageFont.truetype((os.getcwd()+"fonts/BebasNeue-Regular.ttf"),size=40)
+Text = "Living Room"
+
+try:
+    disp = epd7in5_V2.EPD()
+    dispInitRC = disp.init()
+    disp.Clear()
+except:
+    print("Error initialising display")
+
+
+image = PIL.Image.new(mode=‘1’, size=(dispWidth, dispHeight), color=255)
+draw = PIL.ImageDraw.Draw(image)
+
+draw.text((0, 0), Text,
+          font=font, fill=0, align='left')
+try:
+    disp.display(disp.getbuffer(image))
+except:
+    print("Error pushing to display")
+#print(ha.entities.entity_id("light.living_room_desk_lamp"))
