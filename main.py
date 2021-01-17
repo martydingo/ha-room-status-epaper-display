4bc6a56961a6574ddfee40c068c7dbcd4ce5308c
Martin George <marty@dingo.sh>
2021-01-17 03:09:55 +0000

    Fixes and Improvements

--- main.py
+++ main.py
@@ -109,18 +109,18 @@ try:
                       font=climateFont, fill=0, anchor='ls')
         
         ## Power
-        draw.text(((dispWidth/4)*2.3, dispHeight/1.5), "Power",
-                  font=lightsTitleFont, fill=0, anchor='ls')
-        draw.line((((dispWidth/4)*2.2,((dispHeight/1.75)+72)),(((dispWidth/3)*2.1)+80,((dispHeight/1.75)+60))))
-        Power = crawlRoomPowerWatts(roomName)
-        totalWatts = 0
-        for power in Power:
-            try:
-                totalWatts = totalWatts + float(power['state'])
-            except:
-                continue
-        draw.text(((dispWidth/4)*2.4, ((dispHeight/1.5+72))), "Living Room is currently drawing " + str(totalWatts)+" watts",
-                  font=powerFont, fill=0, anchor='ls')
+        # draw.text(((dispWidth/4)*2.3, dispHeight/1.5), "Power",
+        #           font=lightsTitleFont, fill=0, anchor='ls')
+        # draw.line((((dispWidth/4)*2.2,((dispHeight/1.75)+72)),(((dispWidth/3)*2.1)+80,((dispHeight/1.75)+60))))
+        # Power = crawlRoomPowerWatts(roomName)
+        # totalWatts = 0
+        # for power in Power:
+        #     try:
+        #         totalWatts = totalWatts + float(power['state'])
+        #     except:
+        #         continue
+        # draw.text(((dispWidth/4)*2.4, ((dispHeight/1.5+72))), "Living Room is currently drawing " + str(totalWatts)+" watts",
+        #           font=powerFont, fill=0, anchor='ls')
 
         ## Push to Display
         try:
